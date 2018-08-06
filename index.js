144aebad6faf2cc2da8fcd377a7384a962833e7d
Zane Suhadolnik <zanesuhi@gmail.com>
2018-08-06 07:21:23 +0200

    Added !time, !server and !user commands

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -6,11 +7,27 @@ client.on('ready', () => {
 });
 
 //Don't forget to delete the token if you are using a public repo!!!
-client.login('');
+client.login(token);
 
 client.on('message', message => {
-  if (message.content === '!pun') {
+  if (message.content === `${prefix}pun`) {
     message.channel.send('What is a pirates favourite letter? Ayyy it be the sea!');
+  } else if (message.content === `${prefix}time`) {
+    let time = new Date();
+    let hours = time.getHours;
+    let minutes = time.getMinutes;
+    message.channel.send('The current time is: ' + time.getHours + ':' + time.getMinutes);
+  } else if (message.content === `${prefix}server`) {
+    message.channel.send(`Server name: ${message.guild.name}
+    \nTotal members: ${message.guild.memberCount}
+    \nCreated: ${message.guild.createdAt}
+    \nServer Region: ${message.guild.region}`);
+  } else if (message.content === `${prefix}userinfo`) {
+    message.channel.send(`Your username is: ${message.author.username}
+    \nYour ID is: ${message.author.id}
+    \nYou were created: ${message.author.createdAt}
+    \nYour presence: ${message.author.presence.status}
+    \nYour tag: ${message.author.tag}`);
   }
 })
 
